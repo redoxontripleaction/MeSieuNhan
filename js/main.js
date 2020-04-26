139cb1355fd75efd08ec6f3e0bcb512f209100f8
TungHoang <tunghb@vng.com.vn>
2020-04-26 18:47:11 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var check_file_exist = 0
 window.onload = () => {
-
     //open popup
     var modal = document.getElementById("myModal");
 
@@ -23,9 +22,9 @@ window.onload = () => {
 
     const loading_icon_orange = document.getElementById('svg-loading-orange')
     const imageExample = document.getElementById('imageExample')
-    const playDiv = document.getElementById('playDiv')
-    playDiv.style.width = innerWidth * 0.71
-    playDiv.style.height = innerWidth * 0.71
+    // const playDiv = document.getElementById('playDiv')
+    // playDiv.style.width = innerWidth * 0.71
+    // playDiv.style.height = innerWidth * 0.71
 
     //upload image
     const imageCamera = document.getElementById('image-camera')
@@ -59,8 +58,14 @@ window.onload = () => {
     canvas_hd.width = innerWidth * 2
     canvas_hd.height = innerWidth * 2
 
-    canvas.width = innerWidth * 0.71
-    canvas.height = innerWidth * 0.71
+    if(window.innerWidth < 768){
+        canvas.width = innerWidth * 0.65
+        canvas.height = innerWidth * 0.65
+    } else {
+        canvas.width = innerWidth * 0.48
+        canvas.height = innerWidth * 0.48
+    }
+    
 
     //convert base64 to array buffer
     base64ToArrayBuffer = (base64) => {
@@ -211,43 +216,34 @@ window.onload = () => {
     //rotate imagehd for mobile
 
     uploadImage.onchange = (value) => {
+        imageCamera.style.display = 'none'
         handleUploadImage(value)
         handleUploadImageHD(value)
     }
 
     handleUploadImage = (e) => {
         clearInterval(i)
-        // video.src = ''
-        // playDiv.style.display = 'none'
         const source = e.target.files[0]
         const reader = new FileReader();
-        // console.log(source)
         reader.readAsDataURL(source);
-        // loader.style.display = 'block'
-        // canvas.style.display = 'none'
         reader.onloadend = () => {
             loading_icon_orange.style.display = 'unset'
             const preview = new Image();
             preview.src = reader.result
             preview.onload = () => {
                 const exif = EXIF.readFromBinaryFile(base64ToArrayBuffer(reader.result));
-                // document.getElementById('concat').innerText = exif.Orientation
                 switch (exif.Orientation) {
                     case 8:
                         drawRotated(-90, preview)
-                        // drawRotatedHD(-90, preview)
                         break;
                     case 3:
                         drawRotated(180, preview)
-                        // drawRotatedHD(180, preview)
                         break;
                     case 6:
                         drawRotated(90, preview)
-                        // drawRotatedHD(90, preview)
                         break;
                     default:
                         drawRotated(0, preview)
-                        // drawRotatedHD(0, preview)
                         break;
                 }
             }
@@ -257,35 +253,25 @@ window.onload = () => {
 
     handleUploadImageHD = (e) => {
         clearInterval(i)
-        // video.src = ''
-        // playDiv.style.display = 'none'
         const source = e.target.files[0]
         const reader = new FileReader();
-        // console.log(source)
         reader.readAsDataURL(source);
-        // loader.style.display = 'block'
-        // canvas.style.display = 'none'
         reader.onloadend = () => {
             const preview = new Image();
             preview.src = reader.result
             preview.onload = () => {
                 const exif = EXIF.readFromBinaryFile(base64ToArrayBuffer(reader.result));
-                // document.getElementById('concat').innerText = exif.Orientation
                 switch (exif.Orientation) {
                     case 8:
-                        // drawRotated(-90, preview)
                         drawRotatedHD(-90, preview)
                         break;
                     case 3:
-                        // drawRotated(180, preview)
                         drawRotatedHD(180, preview)
                         break;
                     case 6:
-                        // drawRotated(90, preview)
                         drawRotatedHD(90, preview)
                         break;
                     default:
-                        // drawRotated(0, preview)
                         drawRotatedHD(0, preview)
                         break;
                 }
@@ -316,6 +302,9 @@ window.onload = () => {
                     anchor.setAttribute('href', video_preview.src);
                     anchor.setAttribute('download', 'video-redoxon');
                     anchor.click();
+
+                    const obj_url = URL.createObjectURL(blob);
+                    video_preview.src = obj_url;
                     // download(video_preview.src, 'video-redoxon','mp4');
                 }
             } else {
@@ -323,222 +312,9 @@ window.onload = () => {
             }
             
         }
-
-        // var anchor = document.createElement('a');
-
-        // console.log(anchor);
-        // anchor.setAttribute('href', image.src);
-        // anchor.setAttribute('download', 'image.jpeg');
-        // anchor.click();
     }
 
 
-    // //handle rotate for mobile
-    // drawRotatedVideo = (degrees, image) => {
-    //     // alert(image.duration)
-    //     // console.log('drawRotatedVideo')
-    //     const tempCanvas = document.createElement('CANVAS')
-    //     const tempctx = tempCanvas.getContext("2d");
-    //     document.body.appendChild(tempCanvas)
-
-    //     //recorder
-    //     // const chunks = []
-    //     // recorder = new MediaRecorder(tempctx.canvas.captureStream(30));
-    //     // recorder.ondataavailable = (e) => {
-    //     // 	if (e.data.size > 0) {
-    //     // 		chunks.push(e.data);
-    //     // 	}
-    //     // };
-    //     // recorder.onstop = () => {
-    //     // 	const blob = new Blob(chunks, { type: 'video/mp4' });
-    //     // 	const url = window.URL.createObjectURL(blob);
-    //     // 	alert(url)
-    //     // 	const rotatedVideo = document.createElement('VIDEO')
-    //     // 	rotatedVideo.src = url
-    //     // 	rotatedVideo.setAttribute('muted', '')
-    //     // 	rotatedVideo.play()
-    //     // 	rotatedVideo.onloadeddata = () => {
-    //     // 		console.log('onstop')
-    //     // 		renderCanvasVideo(rotatedVideo)
-    //     // 	}
-    //     // };
-    //     // recorder.start()
-    //     // setTimeout(() => {
-    //     // 	recorder.stop()
-    //     // }, image.duration * 1000);
-    //     //recorder
-
-    //     streaming(tempCanvas, image.duration)
-
-    //     tempCanvas.height = 300
-    //     tempCanvas.width = 300
-    //     let tempW = image.videoHeight
-    //     let tempH = image.videoWidth
-    //     let tempImageW;
-    //     let tempImageH;
-    //     switch (degrees) {
-    //         case -90:
-    //             tempImageW = 0
-    //             tempImageH = -tempW
-    //             break;
-    //         case 90:
-    //             tempImageW = -tempH
-    //             tempImageH = 0
-    //             break;
-    //         case 180:
-    //             tempW = image.videoWidth
-    //             tempH = image.videoHeight
-    //             tempImageW = 0
-    //             tempImageH = 0
-    //             break;
-    //         default:
-    //             tempW = image.videoWidth
-    //             tempH = image.videoHeight
-    //             tempImageW = -tempW
-    //             tempImageH = -tempH
-    //             break;
-    //     }
-    //     tempCanvas.width = tempW
-    //     tempCanvas.height = tempH
-    //     tempctx.clearRect(0, 0, tempCanvas.width, tempCanvas.height);
-    //     tempctx.translate(tempCanvas.width, tempCanvas.height);
-    //     tempctx.rotate(degrees * Math.PI / 180);
-    //     setInterval(() => {
-    //         tempctx.drawImage(image, tempImageW, tempImageH);
-    //     }, 20)
-    //     tempctx.save();
-    //     tempctx.restore();
-    // }
-    // //handle rotate for mobile
-
-    // renderImageVideo = (image) => {
-    //     const rwh = image.videoWidth / image.videoHeight
-    //     let newWidth = canvas.width
-    //     let newHeight = newWidth / rwh
-    //     if (rwh > 1) {
-    //         newHeight = canvas.height
-    //         newWidth = newHeight * rwh
-    //     }
-    //     const xOffset = rwh > 1 ? ((canvas.width - newWidth) / 2) : 0;
-    //     const yOffset = rwh <= 1 ? ((canvas.height - newHeight) / 2) : 0;
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     // console.log(rwh, xOffset, yOffset, newWidth, newHeight)
-    //     // streaming(image.duration)
-    //     // i = setInterval(() => {
-    //     ctx.drawImage(image, xOffset, yOffset, newWidth, newHeight);
-    //     ctx.drawImage(frame, 0, 0, canvas.width, canvas.height)
-    //     // }, 20)
-    //     ctx.save();
-    //     ctx.restore()
-    // }
-
-    // //handle add fram
-    // renderCanvasVideo = (image) => {
-    //     const rwh = image.videoWidth / image.videoHeight
-    //     let newWidth = canvas.width
-    //     let newHeight = newWidth / rwh
-    //     if (rwh > 1) {
-    //         newHeight = canvas.height
-    //         newWidth = newHeight * rwh
-    //     }
-    //     const xOffset = rwh > 1 ? ((canvas.width - newWidth) / 2) : 0;
-    //     const yOffset = rwh <= 1 ? ((canvas.height - newHeight) / 2) : 0;
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     // console.log(rwh, xOffset, yOffset, newWidth, newHeight)
-    //     // streaming(image.duration)
-    //     i = setInterval(() => {
-    //         ctx.drawImage(image, xOffset, yOffset, newWidth, newHeight);
-    //         ctx.drawImage(frame, 0, 0, canvas.width, canvas.height)
-    //     }, 20)
-    //     ctx.save();
-    //     ctx.restore()
-    //     // loader.style.display = 'none'
-    //     // canvas.style.display = 'block'
-    // }
-    // //handle add fram
-
-    // const video = document.createElement('VIDEO')
-    // video.setAttribute('autoplay', '')
-    // video.setAttribute('playsinline', '')
-    // video.muted = 'muted'
-    // video.setAttribute('preload', 'auto')
-    // // const playDiv = document.getElementById('playDiv')
-    // console.log(playDiv)
-    // playDiv.onclick = () => {
-    //     playDiv.style.display = 'none'
-    //     video.play()
-    //     renderCanvasVideo(video)
-    // }
-    // video.onended = () => {
-    //     clearInterval(i)
-    //     playDiv.style.display = 'flex'
-    // }
-
-    // //handle upload video
-    // uploadVideo.onchange = (e) => handleUploadVideo(e)
-    // handleUploadVideo = (value) => {
-    //     video.src = ''
-    //     clearInterval(i)
-    //     var source = value.target.files[0]
-    //     const reader = new FileReader();
-    //     if (source) {
-    //         reader.readAsDataURL(source);
-    //         video.src = URL.createObjectURL(source)
-    //         video.onloadeddata = () => {
-    //             playDiv.style.display = 'flex'
-    //             renderImageVideo(video)
-    //         }
-    //     }
-    // }
-    // //handle upload video
-    // //video
-
-    // //recorder
-    // let recordedChunks = [];
-
-    // streaming = (time) => {
-    //     recordedChunks = []
-    //     const stream = canvas.captureStream(25)
-    //     const options = { mimeType: "video/webm; codecs=vp9" };
-    //     const mediaRecorder = new MediaRecorder(stream, options);
-    //     mediaRecorder.ondataavailable = handleDataAvailable;
-    //     mediaRecorder.start();
-    //     setTimeout(event => {
-    //         mediaRecorder.stop();
-    //     }, time * 1000);
-    // }
-
-    // handleDataAvailable = (event) => {
-    //     if (event.data.size > 0) {
-    //         recordedChunks.push(event.data);
-    //         download();
-    //     } else {
-    //         // ...
-    //     }
-    // }
-
-    // download = () => {
-    //     // console.log(recordedChunks)
-    //     var blob = new Blob(recordedChunks, {
-    //         type: "video/webm"
-    //     });
-    //     var url = URL.createObjectURL(blob);
-    //     // alert(url)
-    //     console.log(url)
-    //     const stream = document.createElement('video')
-    //     stream.src = url
-    //     stream.muted = 'muted'
-    //     stream.play()
-    //     document.body.appendChild(stream)
-    //     // var a = document.createElement("a");
-    //     // document.body.appendChild(a);
-    //     // a.style = "display: none";
-    //     // a.href = url;
-    //     // a.download = "test.mp4";
-    //     // a.click();
-    //     // window.URL.revokeObjectURL(url);
-    // }
-    // //recorder
 
     //upload
     const uploadFile = document.getElementById('upload-file')
@@ -550,13 +326,26 @@ window.onload = () => {
             handleUploadImage(value)
             canvas.style.display = 'unset'
             video_preview.style.display = 'none'
+            imageCamera.style.display = 'none'
         } else {
-            // console.log(value.srcElement)
             //video
             handleFiles(value.srcElement.files)
             canvas.style.display = 'none'
             video_preview.style.display = 'unset'
             imageExample.style.display = 'none'
+            imageCamera.style.display = 'none'
+
+            var source = value.target.files[0]
+			var reader = new FileReader();
+			var checkWidthHeighVideo = document.getElementById("checkWidthHeighVideo")
+			reader.onloadend = function () {
+                checkWidthHeighVideo.src = reader.result;
+			}
+			if (source) {
+                reader.readAsDataURL(source);
+			} else {
+				checkWidthHeighVideo.src = "";
+            }
         }
     }
     //upload
@@ -568,6 +357,7 @@ window.onload = () => {
     videoCamera.addEventListener("click", function (e) {
         if (uploadVideo) {
             uploadVideo.click();
+            imageCamera.style.display = 'none'
             canvas.style.display = 'none'
             video_preview.style.display = 'unset'
             imageExample.style.display = 'none'
@@ -578,57 +368,25 @@ window.onload = () => {
 
 
 const cloudName = 'djkc67zly';
-// raw
-// const unsignedUploadPreset = 'redoxon-best';
 
 const unsignedUploadPreset = 'redoxon-new';
 
-
-
-// dropbox = document.getElementById("dropbox");
-// dropbox.addEventListener("dragenter", dragenter, false);
-// dropbox.addEventListener("dragover", dragover, false);
-// dropbox.addEventListener("drop", drop, false);
-
-// // ************************ Drag and drop ***************** //
-// function dragenter(e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-// }
-
-// function dragover(e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-// }
-
-
-// function drop(e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-
-//   var dt = e.dataTransfer;
-//   var files = dt.files;
-
-//   handleFiles(files);
-// }
-
+var video_height
+var video_width
 // *********** Upload file to Cloudinary ******************** //
 function uploadFileVideo(file) {
+    
     var loading_icon_orange = document.getElementById('svg-loading-orange')
+    loading_icon_orange.style.display = 'unset'
     var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
     var xhr = new XMLHttpRequest();
     var fd = new FormData();
     xhr.open('POST', url, true);
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-    // Reset the upload progress bar
-    //    document.getElementById('progress').style.width = 0;
-
+   
     // Update progress (can be used to show progress indicator)
     xhr.upload.addEventListener("progress", function (e) {
-        loading_icon_orange.style.display = 'unset'
-        var progress = Math.round((e.loaded * 100.0) / e.total);
-        // document.getElementById('progress').style.width = progress + "%";
+        
 
         console.log(`fileuploadprogress data.loaded: ${e.loaded},
   data.total: ${e.total}`);
@@ -640,14 +398,12 @@ function uploadFileVideo(file) {
             let video_preview = document.getElementById("myVideo");
             // File uploaded successfully
             var response = JSON.parse(xhr.responseText);
-            // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
             var url = response.secure_url;
             url = url.replace('/upload/', '/upload/fl_attachment/');
             //   alert(response.url);
             console.log(url)
             video_preview.src = url
             check_file_exist = 1
-            //TODO:  add video rendering here(download file)
 
         }
     };
